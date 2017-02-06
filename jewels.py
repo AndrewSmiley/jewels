b0f742831abf195c844af8a87fa77fed23c5bbee
Your Name <andrew.smiley@live.com>
2017-02-06 09:06:00 -0500

    Dfs working?

--- jewels.py
+++ jewels.py
@@ -1,17 +1,17 @@
 __author__ = 'Andrew'
 import copy
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(100000)
 _states = ['D', 'R', 'E']
 _tree = []
 _found_states= []
 _map={0:{'y':0, 'x':0},1:{'y':0, 'x':1}, 2:{'y':0, 'x':2}, 3:{'y':1, 'x':0}, 4:{'y':1, 'x':1}, 5:{'y':1, 'x':2},
       6: {'y': 2, 'x': 0}, 7:{'y':2, 'x':1}, 2:{'y':2, 'x':2}}
+_nodes = []
 class Node(object):
-    def __init__(self,data, pos_value=0, state='open'):
-        # self.parent = parent
+    def __init__(self,data, pos_value=0, parent=None):
+        self.parent = parent
         self.data = data
-        self.state =state
         self.pos_value = pos_value
         self.children = []
 
@@ -82,21 +82,31 @@ def update_grid(grid, pos, bounds):
 
 
 def generate_children(node):
-    for y in range(0, len(grid)):
-        for x in range(0, len(grid[y])):
-            _ = copy.deepcopy(node.data)
-            update_grid(_, {'y': y, 'x': x}, find_bounds({'y': y, 'x': x}))
-            if grid_to_key(_) in _found_states:
-                continue
-            else:
-                child_node = Node( _, {'y': y, 'x': x}, 'open')
+
+    # if node.data == goal_state:
+    #     print 'found goal state'
+    #     return
+    if len(node.children)==0:
+        for y in range(0, len(grid)):
+            for x in range(0, len(grid[y])):
+                _ = copy.deepcopy(node.data)
+                update_grid(_, {'y': y, 'x': x}, find_bounds({'y': y, 'x': x}))
+                # if grid_to_key(_) in _found_states:
+                #     continue
+                # else:
+                child_node = Node( _, {'y': y, 'x': x},  node)
                 node.children.append(child_node)
-                _found_states.append(grid_to_key(_))
+                # _found_states.append(grid_to_key(_))
 
 
-def build_tree(node, base_case):
+
+
+def build_tree(node, base_case, depth=1):
+
     #base case
     # new_node  = copy.copy(node)
+    # if depth == 20:
+    #     return
     if node.data == base_case:
         node.state='closed'
         print "found base"
@@ -104,54 +114,81 @@ def build_tree(node, base_case):
         print "base case %s" %(base_case)
 
         return
+    #
+    # for child_node in node.children:
+    #     build_tree(child_node, base_case)
+    #     build the children for this level
+    generate_children(node, base_case)
+
+    # if len(node.children)==0:
+        # node.state == 'closed'
 
     for child_node in node.children:
+        # _found_states.append(grid_to_key(child_node))
         build_tree(child_node, base_case)
-        #build the children for this level
-
-    generate_children(node)
-    if len(node.children)==0:
-        node.state == 'closed'
-
-    for child_node in node.children:
-        build_tree(child_node, base_case)
-
-def dfs_search(node, goal_state, next_moves):
-    #base
-    if len(node.children) == 0 and node.data != goal_state:
-        return False
-    elif len(node.children) == 0  and node.data== goal_state:
-        # next_moves.append(node.pos_value)
-        return True
-
-    for child_node in node.children:
-        found = dfs_search(child_node, goal_state, next_moves)
-        if found:
-            next_moves.append(child_node.pos_value)
-            return True
-
-    #basically if there is no match in the tree
-    return False
-
-
 
 
 
 #ok create our grid
 grid = create_grid()
 # def __init__(self, data, pos_value=0, state='open'):
-n = Node(grid, 0, 'open' )
+n = Node(grid, 0, None)
+# _found_states.append(grid_to_key(n.data))
 goal_state_0 =[]
 for i in range(0, 3):
     goal_state_0.append([])
     for j in range(0, 3):
         goal_state_0[i].append('E')
+goal_state_1=[['E', 'E', 'R', ], ['E', 'R', 'E'], ['R', 'E', 'R']]
+
+class Node(object):
+    def __init__(self,data, pos_value=0, parent=None):
+        self.parent = parent
+        self.data = data
+        self.pos_value = pos_value
+        self.children = []
+
+    def add_child(self, child):
+        self.children.append(child)
+
+moves =[]
+def dfs(node, goal_state, stack, depth):
+    global moves
+    if len(moves) >0:
+        return
+
+    if grid_to_key(node.data) in _found_states:
+        return
+
+    if node.data == goal_state:
+        n = node
+        while n.parent is not None:
+            moves.append(pos_to_value(n.pos_value))
+            n = n.parent
+        print "size: %s" %(len(stack))
+        print "found len %s %s "%(len(moves), moves)
+        return
+    generate_children(node)
+
+    # stack.append(node)
+    if grid_to_key(node.data) in _found_states:
+        stack.pop()
+        print len(stack)
+        return
+
+    else:
+        _found_states.append(grid_to_key(node.data))
+        for child in stack[-1].children:
+            if grid_to_key(child.data) in _found_states:
+                continue
+            else:
+                stack.append(child)
+                dfs(child, goal_state, stack, depth+1)
+    return
+
+print dfs(n, goal_state_0, [n], 0)
 
 
-build_tree(n,goal_state_0)
-next_moves = []
-found = dfs_search(n, goal_state_0, next_moves)
-print "found: %s" %(found)
-print next_moves
+
 
 print "Test"
