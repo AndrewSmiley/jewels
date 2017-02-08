b95f472c14c6ab5fc143171e2e3cb678fed2f8b4
Andrew Smiley <andrew.smiley@ge.com>
2017-02-07 20:50:44 -0500

    finished bfs, adding docs

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # jewels
-# jewels
-# jewels
+
+A short report that describes who in your group implemented which portions (if you
+worked in a group), what you learned from the assignment, how easy or challenging the
+assignment was
+
+An explanation of your heuristic and whether you felt it improved the search and whether
+a better heuristic might help
+
+```
+def hueristic(node, goal_state):
+    total_score = 0
+    #well, if this node is our goal give it the highest score
+    if node.data == goal_state:
+        return pow(9, 5)
+    generate_children(node)
+    #if the children of this node contain the solution, it's the next best solution
+    if len (filter(lambda x: x.data == goal_state, node.children)) >0:
+        return pow(9,4)
+    #kill the children \M/
+    del node.children[:]
+    for y in range(0, len(node.data)):
+        for x in range(0, len(node.data[y])):
+            distance_to_goal = get_space_distance(node.data[y][x], goal_state[y][x])
+            if distance_to_goal == 1:
+                total_score = total_score+4
+            else:
+                total_score = total_score+distance_to_goal
+```
--- jewels.py
+++ jewels.py
@@ -3,7 +3,6 @@ import copy
 import sys
 sys.setrecursionlimit(100000)
 _states = ['D', 'R', 'E']
-_tree = []
 _found_states= []
 _map={0:{'y':0, 'x':0},1:{'y':0, 'x':1}, 2:{'y':0, 'x':2}, 3:{'y':1, 'x':0}, 4:{'y':1, 'x':1}, 5:{'y':1, 'x':2},
       6: {'y': 2, 'x': 0}, 7:{'y':2, 'x':1}, 2:{'y':2, 'x':2}}
@@ -45,8 +44,6 @@ def create_grid():
     return grid
 
 
-def hueristic(grid):
-    pass
 
 def find_bounds(pos):
     #up down left right
@@ -83,63 +80,13 @@ def update_grid(grid, pos, bounds):
 
 def generate_children(node):
 
-    # if node.data == goal_state:
-    #     print 'found goal state'
-    #     return
     if len(node.children)==0:
-        for y in range(0, len(grid)):
-            for x in range(0, len(grid[y])):
+        for y in range(0, len(node.data)):
+            for x in range(0, len(node.data[y])):
                 _ = copy.deepcopy(node.data)
                 update_grid(_, {'y': y, 'x': x}, find_bounds({'y': y, 'x': x}))
-                # if grid_to_key(_) in _found_states:
-                #     continue
-                # else:
                 child_node = Node( _, {'y': y, 'x': x},  node)
                 node.children.append(child_node)
-                # _found_states.append(grid_to_key(_))
-
-
-
-
-def build_tree(node, base_case, depth=1):
-
-    #base case
-    # new_node  = copy.copy(node)
-    # if depth == 20:
-    #     return
-    if node.data == base_case:
-        node.state='closed'
-        print "found base"
-        print "node data %s" %(node.data)
-        print "base case %s" %(base_case)
-
-        return
-    #
-    # for child_node in node.children:
-    #     build_tree(child_node, base_case)
-    #     build the children for this level
-    generate_children(node, base_case)
-
-    # if len(node.children)==0:
-        # node.state == 'closed'
-
-    for child_node in node.children:
-        # _found_states.append(grid_to_key(child_node))
-        build_tree(child_node, base_case)
-
-
-
-#ok create our grid
-grid = create_grid()
-# def __init__(self, data, pos_value=0, state='open'):
-n = Node(grid, 0, None)
-# _found_states.append(grid_to_key(n.data))
-goal_state_0 =[]
-for i in range(0, 3):
-    goal_state_0.append([])
-    for j in range(0, 3):
-        goal_state_0[i].append('E')
-goal_state_1=[['E', 'E', 'R', ], ['E', 'R', 'E'], ['R', 'E', 'R']]
 
 class Node(object):
     def __init__(self,data, pos_value=0, parent=None):
@@ -151,22 +98,17 @@ class Node(object):
     def add_child(self, child):
         self.children.append(child)
 
-moves =[]
+
 def dfs(node, goal_state, stack, depth):
     global moves
     if len(moves) >0:
         return
-
-    if grid_to_key(node.data) in _found_states:
-        return
-
     if node.data == goal_state:
         n = node
         while n.parent is not None:
             moves.append(pos_to_value(n.pos_value))
             n = n.parent
-        print "size: %s" %(len(stack))
-        print "found len %s %s "%(len(moves), moves)
+        print "Found %s in Depth First Search: %s moves of %s states visited"%(goal_state,len(moves), len(_found_states))
         return
     generate_children(node)
 
@@ -185,6 +127,40 @@ def dfs(node, goal_state, stack, depth):
                 stack.append(child)
                 dfs(child, goal_state, stack, depth+1)
     return
+
+def bfs(node, goal_state):
+    global moves
+    if len(moves) >0:
+        return
+
+    if grid_to_key(node.data) in _found_states:
+        return
+
+    if node.data == goal_state:
+        n = node
+        while n.parent is not None:
+            moves.append(pos_to_value(n.pos_value))
+            n = n.parent
+        # print "size: %s" %(len(stack))
+        print "Found %s in Best First Search:%s moves of %s states visited"%(goal_state, len(moves), len(_found_states))
+        return
+
+    # stack.append(node)
+    if grid_to_key(node.data) in _found_states:
+        return
+    else:
+        generate_children(node)
+        #sort the children by largest hueristic value
+        node.children = sorted(node.children, key=lambda x: hueristic(x, goal_state), reverse=True)
+        _found_states.append(grid_to_key(node.data))
+        for child in node.children:
+            if grid_to_key(child.data) in _found_states:
+                continue
+            else:
+
+                bfs(child, goal_state)
+    return
+
 
 #just a little utility function to generate the number of steps to the goal state at that space
 def get_space_distance(space, goal):
@@ -213,12 +189,55 @@ def hueristic(node, goal_state):
 
 
 
-def bfs(node, goal_state, stack, depth):
 
 
-print dfs(n, goal_state_0, [n], 0)
+moves =[]
+#ok create our grid
+grid = create_grid()
+# def __init__(self, data, pos_value=0, state='open'):
 n = Node(grid, 0, None)
+# _found_states.append(grid_to_key(n.data))
+goal_state_0 =[]
+for i in range(0, 3):
+    goal_state_0.append([])
+    for j in range(0, 3):
+        goal_state_0[i].append('E')
+
+goal_state_1=[['E', 'E', 'R', ], ['E', 'R', 'E'], ['R', 'E', 'R']]
+goal_state_2=[['R', 'R', 'E', ], ['R', 'E', 'R'], ['E', 'R', 'R']]
+goal_state_3=[['R', 'D', 'R', ], ['D', 'R', 'D'], ['R', 'D', 'R']]
 
 
-
-print "Test"
+dfs(n, goal_state_0, [n], 0)
+#reset moves and found states
+moves= []
+_found_states =[]
+new_node = Node(grid, 0, None)
+bfs(new_node, goal_state_0)
+moves= []
+_found_states =[]
+n = Node(grid, 0, None)
+dfs(n, goal_state_1, [n], 0)
+#reset moves and found states
+moves= []
+_found_states =[]
+new_node = Node(grid, 0, None)
+bfs(new_node, goal_state_1)
+moves= []
+_found_states =[]
+n = Node(grid, 0, None)
+dfs(n, goal_state_2, [n], 0)
+#reset moves and found states
+moves= []
+_found_states =[]
+new_node = Node(grid, 0, None)
+bfs(new_node, goal_state_2)
+moves= []
+_found_states =[]
+n = Node(grid, 0, None)
+dfs(n, goal_state_3, [n], 0)
+#reset moves and found states
+moves= []
+_found_states =[]
+new_node = Node(grid, 0, None)
+bfs(new_node, goal_state_3)
